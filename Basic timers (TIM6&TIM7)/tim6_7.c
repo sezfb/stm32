ef8c60c63366e1249ea2c0bde40f44f580302d10
sezfb <cofhdz.uzf@gmail.com>
2026-07-03 01:03:36 +0300

    mmm

---
+++ Basic timers (TIM6&TIM7)/tim6_7.c
@@ -0,0 +1,36 @@
+#include "tim6_7.h"
+
+void tim6_init(void)
+{   
+    // 84,000,000 / (8399 + 1) = 10,000 Hz
+    TIM6_PSC = 8399;
+
+    // частота × время = количество тиков
+    // 10,000 Hz * 5с = 50,000 тиков
+    // 
+    TIM6_ARR = 49999; // начало с 0, по этому 49999 + 1
+
+
+    TIM6_CR1 |= (1 << 7);
+    TIM6_CR1 &= ~(1 << 4);
+    TIM6_CR1 |= (1 << 3);
+    TIM6_CR1 &= ~(1 << 2);
+    TIM6_CR1 &= ~(1 << 0);
+
+    TIM6_DIER |= (1 << 8); // DMA, само по себе ничего не дает
+    // надо включить NVIC, чтобы CPU реагировал на прерывания от таймера
+}
+
+void tim6_ON(void)
+{  TIM6_CR1 |= (1 << 0);  }
+
+void tim6_OFF(void)
+{  TIM6_CR1 &= ~(1 << 0);   }
+
+void tim6_STATUS(void)
+{
+    uint32_t status = TIM6_CNT;
+
+    printf("TIM6_CNT = %lu\r\n", (unsigned long)status);
+}
+
---
+++ tims/f.md
@@ -0,0 +1,22 @@
+STM32F446xx has :
+
+- Advanced-control timers (TIM1&TIM8)
+The advanced-control timers (TIM1&TIM8) consist of a 16-bit auto-reload counter driven by
+a programmable prescaler.
+
+- General-purpose timers  (TIM2 to TIM5)
+The general-purpose timers consist of a 16-bit or 32-bit auto-reload counter driven by a
+programmable prescaler.
+
+- General-purpose timers  (TIM9 to TIM14)
+The TIM9 to TIM14 general-purpose timers consist of a 16-bit auto-reload counter driven by
+a programmable prescaler.
+
+- Basic timers            (TIM6&TIM7)
+The basic timers TIM6 and TIM7 consist of a 16-bit auto-reload counter driven by a
+programmable prescaler.
+
+14 in summa.
+
+|= (1 << n) — установить бит n в 1.
+&= ~(1 << n) — сбросить бит n в 0.
